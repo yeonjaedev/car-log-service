412a80f1fb8f302bda300c47687a899be3fcb745
yeonjaedev <yjae47@gmail.com>
2022-10-15 17:20:07 +0900

    [ADD] 유저 이름에 유니크한 값 주기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
@@ -14,12 +14,20 @@ export class AuthService {
     async createUser (authCredentialDto:AuthCredentialDto):Promise<void> {
         const {username,password} = authCredentialDto;
         const user = this.userRepository.create({username,password});
-        await this.userRepository.save(user);
+
+        try{
+            await this.userRepository.save(user);
+        } catch (error) {
+            if(error.code === '23505'){
+                throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 
     async signUp(authCredentialDto:AuthCredentialDto):Promise<void> {
         return this.createUser(authCredentialDto);
-
     }
 
 }
