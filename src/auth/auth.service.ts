b457aa770cf22639ef3e64083f4662f476109996
yeonjaedev <yjae47@gmail.com>
2022-10-15 17:23:23 +0900

    [ADD] 비밀번호 암호화 ( salt + 비밀번호를 해시로 암호화해서 저장 )

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
-
+import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class AuthService {
     constructor(
@@ -13,7 +13,9 @@ export class AuthService {
 
     async createUser (authCredentialDto:AuthCredentialDto):Promise<void> {
         const {username,password} = authCredentialDto;
-        const user = this.userRepository.create({username,password});
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password,salt)
+        const user = this.userRepository.create({username,password:hashedPassword});
 
         try{
             await this.userRepository.save(user);
