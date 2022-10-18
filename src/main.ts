113104806ae7a2d73763f41a64ab417739921eee
yeonjaedev <yjae47@gmail.com>
2022-10-18 20:55:47 +0900

    Fix settings for heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
-  await app.listen(port);
-  logger.log(`Application running on Port ${port}`)
+  // const port = 3000;
+  // await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
+  logger.log(`Application running on Port ${process.env.PORT || 3000}`)
 }
 bootstrap();
